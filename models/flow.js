6c486fdd8be5e4398e490310e6368713f38ce333
Muhammad Hamza Saleemi <muhammadhamzasaleemi5@gmail.com>
2025-08-31 22:35:44 +0500

    status added

--- models/flow.js
+++ models/flow.js
@@ -22,6 +22,7 @@ const flowSchema = new mongoose.Schema(
   {
     clientId: { type: String, required: true },   // Here we link it to a specific client
     name: { type: String, required: true },
+    status: { type: String, required: true },
     nodes: [nodeSchema],
     edges: [edgeSchema]
   },
