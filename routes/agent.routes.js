cead9defc306c897b46294f97defea70fab88700
M.Hamza Saleemi <developer5.grafdom@gmail.com>
2025-09-02 14:37:27 +0500

    loginAgentController

--- routes/agent.routes.js
+++ routes/agent.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createAgentController, updateAgentController } from '../controllers/agent.controller.js';
+import { createAgentController, updateAgentController,loginAgentController } from '../controllers/agent.controller.js';
 
 const router = Router();
 
