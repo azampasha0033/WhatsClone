40ff2d46bfb5d5475100c891774d77c2a6e9e79f
Muhammad Hamza Saleemi <muhammadhamzasaleemi5@gmail.com>
2025-09-13 16:54:42 +0500

    set now please

--- utils/validateApiKey.js
+++ utils/validateApiKey.js
@@ -6,8 +6,15 @@ import { ClientModel } from '../db/clients.js';
  * @throws {Error} If the API key is invalid.
  */
 export async function validateApiKey(clientId, apiKey) {
+
+    console.log('client id 1'+clientId);
+    console.log('API KEY HERE 1'+apiKey);
   const client = await ClientModel.findOne({ clientId });
+
+
   if (!client || client.apiKey !== apiKey) {
-    throw new Error('Invalid API key');
+    throw new Error('Invalid API key here ');
   }
 }
+
+
